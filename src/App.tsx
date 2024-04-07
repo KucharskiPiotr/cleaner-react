30a1dbab740a4cc85d94a0f23f529ade5d48b167
KucharskiPiotr <29356216+KucharskiPiotr@users.noreply.github.com>
2024-04-07 15:16:04 +0200

    Remove other versions

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import './App.css'
 import Blog from "./blog/blog.tsx";
-import BetterBlog from "./blog/better/blog.tsx";
-import BestBlog from "./blog/best/blog.tsx";
 
 function App() {
   return (
     <>
-      {/*<Blog />*/}
-      {/*<BetterBlog />*/}
-      <BestBlog />
+      <Blog />
     </>
   )
 }
